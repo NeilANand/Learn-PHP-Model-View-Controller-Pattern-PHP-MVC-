b11d73e1f8238b2ab68770f57ad5ff4ce6744fd8
Neil Nand <me@neilnand.co.uk>
2015-11-20 23:00:44 +0000

    Added method to parse URL

--- app/core/App.php
+++ app/core/App.php
@@ -1,6 +1,13 @@
 <?php
 class App{
   public function __construct(){
-    echo "Hello!";
+    $url = $this->parseUrl();
+  }
+
+  protected function parseUrl(){
+    if(isset($_GET["url"])){
+      //Splits URL by / & trims off any extra /s (so /test//////testing would be seen as /test/testing by the explode function) and makes sures the URL doesn't contain anything malicious
+      return explode("/", filter_var(rtrim($_GET["url"], "/"), FILTER_SANITIZE_URL));
+    }
   }
 }
