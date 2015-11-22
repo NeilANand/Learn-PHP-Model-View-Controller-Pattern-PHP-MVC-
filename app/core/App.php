b658ee50a61e065a73f033f9550610e0ab182e7a
Neil Nand <me@neilnand.co.uk>
2015-11-22 03:15:42 +0000

    Takes parsed URL & calls the correct controller & method

--- app/core/App.php
+++ app/core/App.php
@@ -1,7 +1,35 @@
 <?php
 class App{
+  protected $controller = "home";
+  protected $method = "index";
+  protected $params = [];
+
   public function __construct(){
     $url = $this->parseUrl();
+
+    //$url[0] is the controller name
+    if(file_exists("../app/controllers/" . ucfirst($url[0]) . ".php")){
+      $this->controller = $url[0];
+      unset($url[0]);
+    }
+
+    require "../app/controllers/" . ucfirst($this->controller) . ".php";
+
+    //Set the controller parameter of this object to an instantiated controller object
+    $this->controller = new $this->controller;
+
+    //$url[1] is the method on the controller to call
+    if(isset($url[1])){
+      if(method_exists($this->controller, $url[1])){
+        $this->method = $url[1];
+        unset($url[1]);
+      }
+    }
+
+    //Check if the URL had any extra parameters to be added
+    $this->params = !empty($url) ? array_values($url) : [];
+
+    call_user_func_array([$this->controller, $this->method], $this->params);
   }
 
   protected function parseUrl(){
