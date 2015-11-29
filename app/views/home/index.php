f6b8960cd6a99450d47051765bfd9c37db3b67c8
Neil Nand <me@neilnand.co.uk>
2015-11-29 19:44:06 +0000

    Created view template for home index

--- app/views/home/index.php
+++ app/views/home/index.php
@@ -1,1 +1,20 @@
-Hello {{ name }}
+<!DOCTYPE html>
+<html lang ="en">
+  <head>
+    <meta charset="utf-8">
+
+    <title>Website</title>
+  </head>
+
+  <body>
+    <header class="main">
+      <h1>Welcome to the home/index view</h1>
+    </header>
+
+    <p>Below is an example of how you pass parameters into the application</p>
+
+    <code>/home/index/[name]/[mood]</code>
+
+    <p>{{ name }} is {{ mood }}</p>
+  </body>
+</html>
