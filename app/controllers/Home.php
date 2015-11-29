48641edb3e99d9d16922a85358f53380b4168419
Neil Nand <me@neilnand.co.uk>
2015-11-29 03:54:48 +0000

    Removed mood

--- app/controllers/Home.php
+++ app/controllers/Home.php
@@ -1,9 +1,8 @@
 <?php
 class Home extends Controller{
-  public function index($name = "", $mood = ""){
+  public function index($name = ""){
     $this->view("home/index", [
-      "name" => $name,
-      "mood" => $mood,
+      "name" => $name
     ]);
   }
 }
