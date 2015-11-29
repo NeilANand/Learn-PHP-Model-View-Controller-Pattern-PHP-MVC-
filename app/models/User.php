492ca33ab7de9bd6a52a92e7525568874b85133f
Neil Nand <me@neilnand.co.uk>
2015-11-29 10:55:26 +0000

    Replaced property with explicit table name reference

--- app/models/User.php
+++ app/models/User.php
@@ -2,5 +2,5 @@
 use Illuminate\Database\Eloquent\Model as Eloqent;
 
 class User extends Eloqent{
-  public $name;
+  protected $table = "users"; //Not needed as Eloqent will automatically associate the User class with the users table but I like to be explicit
 }
