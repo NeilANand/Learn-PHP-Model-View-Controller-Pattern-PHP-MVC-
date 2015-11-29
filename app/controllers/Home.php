f4985eb536a9ea7a51d160d1ba591f2ecb300db3
Neil Nand <me@neilnand.co.uk>
2015-11-29 19:42:33 +0000

    Added mood parameter back into method

--- app/controllers/Home.php
+++ app/controllers/Home.php
@@ -1,12 +1,13 @@
 <?php
 class Home extends Controller{
-  public function index($name = ""){
+  public function index($name = "", $mood = ""){
     $user = $this->model("user");
 
     $user->name = $name;
 
     $this->view("home/index", [
-      "name" => $user->name
+      "name" => $user->name,
+      "mood" => $mood
     ]);
   }
 }
