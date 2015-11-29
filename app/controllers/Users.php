2cd4ab3b8faac5af74522dd83429ce9297c2fdb7
Neil Nand <me@neilnand.co.uk>
2015-11-29 11:00:56 +0000

    Set default value for user variable

--- app/controllers/Users.php
+++ app/controllers/Users.php
@@ -3,6 +3,8 @@ class Users extends Controller{
   public function get($id = null){
     $userModel = $this->model("user");
 
+    $user = null;
+
     if($id){
       $user = $userModel->find($id);
     }
