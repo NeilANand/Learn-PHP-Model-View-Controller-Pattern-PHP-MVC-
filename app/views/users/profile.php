5b2f2c482df97421a6964f30849cafd9e3aae859
Neil Nand <me@neilnand.co.uk>
2015-11-29 11:04:52 +0000

    Created basic HTML output for user profile (no real data yet)

--- app/views/users/profile.php
+++ app/views/users/profile.php
@@ -1,1 +1,13 @@
-User profile
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+
+    <title>User profile</title>
+  </head>
+
+  <body>
+    <h2>x's profile</h2>
+    <p>Email: y</p>
+  </body>
+</html>
