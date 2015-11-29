c9f86333c9d27bccf6c1bef351ac51cc92ead9f8
Neil Nand <me@neilnand.co.uk>
2015-11-29 10:57:11 +0000

    var_dump specific user entry in table

--- app/controllers/Users.php
+++ app/controllers/Users.php
@@ -3,6 +3,9 @@ class Users extends Controller{
   public function get($id = null){
     $userModel = $this->model("user");
 
-    var_dump($userModel);
+    if($id){
+      $user = $userModel->find($id);
+      var_dump($user);
+    }
   }
 }
